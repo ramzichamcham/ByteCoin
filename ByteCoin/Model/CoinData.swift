f8aaaba0815bf81330c527281ead6a40acfed699
Ramzi Chamcham <ramzichamcham@gmail.com>
2020-07-14 11:29:15 -0400

    Project complete

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ramzi chamcham on 2020-02-03.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable{
+    let rate: Double
+}
